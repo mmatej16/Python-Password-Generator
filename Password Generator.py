bf26ab9213fb5ebfbfea37e285810ced1876ca83
Matej <120265652+mmatej16@users.noreply.github.com>
2023-01-19 22:36:16 +0100

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -7,7 +7,7 @@ symbols = "[]{}\|:;<,>.?/_-+=!@#$%^&*()"
 
 all = lower + upper + numbers +symbols
 print("Password Generator")
-print("mmatej16 on github")
+print("@mmatej16 on Github")
 length = int(input('\nEnter the length of the password(s): '))
 num = int(input('\nEnter how many passwords you want to generate?: '))
 print("")
