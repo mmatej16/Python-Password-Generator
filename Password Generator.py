79d75111054fdce88529ba575e014b93291dfc11
Matej <120265652+mmatej16@users.noreply.github.com>
2023-01-19 22:24:00 +0100

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,18 @@
+import random
+
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
+numbers = "0123456789"
+symbols = "[]{}\|:;<,>.?/_-+=!@#$%^&*()"
+
+all = lower + upper + numbers +symbols
+print("Password Generator")
+print("mmatej16 on github")
+length = int(input('\nEnter the length of the password(s): '))
+num = int(input('\nEnter how many passwords you want to generate?: '))
+print("")
+print("Your generated password(s) are:")
+print("")
+for n in range (0, num):
+    password = "".join(random.sample(all,length))
+    print(password)
